f0b857d5d8d2eebaa984fc4b7e16b1ced1d32520
Karzan <karzan.maher@gmail.com>
2024-12-08 00:02:56 +0100

    expirimental faster version

--- main.c
+++ main.c
@@ -5,9 +5,10 @@
 #include "fft.h"
 #include "cordic-math.h"
 
-#define LCD_HEIGHT 79
+#define LCD_HEIGHT 80
+#define LCD_HEIGHT_HALF 40
 #define LCD_WIDTH 160
-#define FFT_WIDTH 128
+#define ARRAY_SIZE 128			// FFT needs 2^k samples, displaying IFFT limited by LCD_WIDTH 
 
 #define DRAW_X_Y(COLOR) \
 		LCD_DrawLine(0, 0, 0, LCD_HEIGHT, COLOR); \
@@ -15,83 +16,58 @@
 
 void shiftArray(Complex arr[], uint8_t n, int dc);
 void visualize_fft(Complex *x, int16_t N);
-void visualize_ifft(Complex *x, int16_t N, int mean);
-
-/************************************
-Differences:
-- not show continious reading 
-
-Structure:
-- Sample as fast as possbile 
-- FFT and visualise for x seconds
-- IFFT and visualise for X seconds
-- Start samping again
-***********************************/
+void visualize_ifft(Complex *x, int16_t N);
 
 int main(void){
-  uint8_t len = 0;								 				// max value will be 128
-  int16_t adcr = 0, tmpr = 40;
-	uint16_t mean = 0;														// calc. DC component with average value
-	static int g = 0b00000101;							// 80/4096 => 0,01953125 << 8
+	int16_t adcr = 0, tmpr = 0;
+	static int8_t sf_adc_lcd = 0b00000101;		// scaling factor Q8: 80/4096 => 0,01953125 << 8
 
-	Complex samples[FFT_WIDTH] = { {0, 0} }; 	// initialize all samples to 0 first
+	Complex samples[ARRAY_SIZE] = { {0, 0} };		 	// initialize all samples to 0 first
+	Complex* pSamples = samples;									// pointer to samples array
+	int16_t count = ARRAY_SIZE;						 			// start with last index for backfilling array
+
 
   Lcd_SetType(LCD_INVERTED);                // or use LCD_INVERTED!
   Lcd_Init();
   ADC3powerUpInit(1);                       // Initialize ADC0, Ch3 & Ch16
   LCD_Clear(BLACK);
 
-	//int current_time =0, stop_time=0; 	// for get_timer_value() function
   while (1) {                             
 		do{
-			delay_until_1us(25);
+			//delay_until_1us(500); // using 500 us when working without function generator
+			delay_until_1us(10);
 
 			if (adc_flag_get(ADC0,ADC_FLAG_EOC)==SET) {   // ...ADC done?
-				adcr = adc_regular_data_read(ADC0);         // ......get data   0<->4095
-				tmpr = (int16_t)(adcr * g) >> 8; 					  // scale adc value in range of 0-LCD_HEIGHT with divsion 4096
-				//samples[len-1].real = tmpr;                     // buffer error after 2 iterations?
-				samples[0].real = tmpr;
-				shiftArray(samples, len, mean);
+				adcr = adc_regular_data_read(ADC0);         // ......get data   0<->4096
+				tmpr = (int16_t)(adcr * sf_adc_lcd) >> 8;	  // scale adc value in range of 0-LCD_HEIGHT with divsion 4096
+				pSamples[(--count)].real = tmpr;            // buffer error after 2 iterations?
 				adc_flag_clear(ADC0, ADC_FLAG_EOC);         // ......clear IF
-				len++;
 			}
 
 			adc_software_trigger_enable(ADC0,  //Trigger another ADC conversion!
 																		ADC_REGULAR_CHANNEL);
 
 			while(!delay_finished()); 		// wait until iteration done
-		} while(len<128);						// do this until 128 samples
+		}while(count>0);					// do this until 128 samples
 	
-		for (int i = 0; i < len; i++){
-			mean += i; 
-		}
-		mean = mean / len;
+		// test fft visuals
+		fft(samples, ARRAY_SIZE); 
+		visualize_fft(samples, ARRAY_SIZE);
+		delay_1ms(1000);
 		
-		if (len==128){							// <-- unnecesary condition (?)
-			// test fft visuals
-			LCD_Clear(BLACK);
-			fft(samples, len); 
-			visualize_fft(samples, len);
-			delay_1ms(1000);
-			
-			// test inverse visuals
-			LCD_Clear(BLACK);
-			inverse_fft(samples, len);
-			visualize_ifft(samples, len, mean);
-			delay_1ms(1000);
-			
-			LCD_Clear(BLACK);
-			for (int16_t i = 0; i < len; i++){
-				samples[i].real = 0; 
-			}
-			len=1;
+		// test inverse visuals
+		inverse_fft(samples, ARRAY_SIZE);
+		visualize_ifft(samples, ARRAY_SIZE);
+		delay_1ms(1000);
+		
+		// clear LCD, counter and array values
+		LCD_Clear(BLACK);
+		for (int16_t i=0; i<ARRAY_SIZE; i++) {
+			pSamples[i].real = 0;
+			pSamples[i].imag = 0;
 		}
-	}
-}
-
-void shiftArray(Complex arr[], uint8_t n, int dc){
-	for (int8_t i=n-1; i>=0; i--){																	
-		arr[i+1] = arr[i];		                       	// shift 1 step
+		count = ARRAY_SIZE;
+		// while (count < ARRAY_SIZE) pSamples[(count++)].real = 0;
 	}
 }
 
@@ -112,14 +88,13 @@ void visualize_fft(Complex *x, int16_t N){
 
 	// scale the magnitudes according to the max for the LCD
 	for (int16_t i = 0; i<N; i++){
-		// fixed division with max magnitude
 		magnitudes[i] = (magnitudes[i] << CORDIC_MATH_FRACTION_BITS) / max_magnitude;
-		// fixed multiplication with half of LCD height
-		magnitudes[i] = (magnitudes[i] >> CORDIC_MATH_FRACTION_BITS) * (LCD_HEIGHT/2);
-		//magnitudes[i] = floor_log2_32(magnitudes[i]); 		// log scale magnitude
+		magnitudes[i] = (magnitudes[i] * LCD_HEIGHT_HALF) >> CORDIC_MATH_FRACTION_BITS;
+		//magnitudes[i] = floor_log2_32(1 + magnitudes[i]);
 	}
 	
 	// draw x-y axes 
+	LCD_Clear(BLACK);
 	DRAW_X_Y(WHITE);
 
 	for (int16_t i=0; i<N; i++){
@@ -127,9 +102,18 @@ void visualize_fft(Complex *x, int16_t N){
 	}
 }
 
-void visualize_ifft(Complex *x, int16_t N, int DC){
+void visualize_ifft(Complex *x, int16_t N){
+	// calculate the dc offset
+	int16_t dc = 0;
+	for (int16_t i = 0; i<N; i++){
+		dc += x[i].real;						
+	}
+	dc /= N;			
+	
+	LCD_Clear(BLACK);
 	DRAW_X_Y(WHITE);
 	for (int16_t i=0; i<N; i++){
-		LCD_DrawPoint(i, DC-(x[i].real), YELLOW);								  // inverse FFT of input signal
+		LCD_DrawPoint(i, (40+dc) - x[i].real, YELLOW);
 	}
+	LCD_Wait_On_Queue(); 		// wait until LCD finished drawing
 }
